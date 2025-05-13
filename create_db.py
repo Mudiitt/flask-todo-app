ef61888b403b5c3f0f21b737073ed0560e9b64e3
Mudit <muditsingh942@gmail.com>
2025-05-14 01:19:10 +0530

    all files added

---
+++ create_db.py
@@ -0,0 +1,9 @@
+from app import app, db
+
+with app.app_context():
+    try:
+        db.create_all()
+        print("Tables created successfully.")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
